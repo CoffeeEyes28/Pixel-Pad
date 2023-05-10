95aa2d49d76a878c5c17146a5fcff914a7cc841a
CoffeeEyes28 <liv@Olivias-MacBook-Air.local>
2023-05-10 08:48:04 -0400

    changed color options to palette and choice system

--- pixel-pad/src/components/Pad.js
+++ pixel-pad/src/components/Pad.js
@@ -1,119 +1,118 @@
-import { useState } from 'react';
-import styles from '../styles/Pad.module.css';
+import { useState } from "react";
+import styles from "../styles/Pad.module.css";
 const Pad = () => {
-const gridSize = 24
-const squares = Array(gridSize * gridSize).fill(null);
-const [color, setColor] = useState('white');
-const [clicked, setClicked] = useState(false)
+  const gridSize = 24;
+  const squares = Array(gridSize * gridSize).fill(null);
+  const [color, setColor] = useState("white");
+  const [clicked, setClicked] = useState(false);
+  const [choices, setChoices] = useState([
+    "white",
+    "black",
+    "green",
+    "blue",
+    "red",
+    "yellow",
+    "purple",
+    "orange",
+  ]);
+  const [palette, setPalette] = useState("default");
 
-const handleClick = (index) => {
-    const square = document.getElementById(`square-${index}`)
+  const handleClick = (index) => {
+    const square = document.getElementById(`square-${index}`);
 
-    square.style.backgroundColor= color
-}
+    square.style.backgroundColor = color;
+  };
 
-const handleClear = () => {
-    
-    const allSquares = document.getElementsByClassName(styles.square)
+  const handleClear = () => {
+    const allSquares = document.getElementsByClassName(styles.square);
 
-    for(let i =0; i < allSquares.length; i++){
-        const square = allSquares[i]
+    for (let i = 0; i < allSquares.length; i++) {
+      const square = allSquares[i];
 
-        square.style.backgroundColor = 'white'
-        
+      square.style.backgroundColor = "white";
+    }
+  };
+
+  const handlePreview = () => {
+    const allSquares = document.getElementsByClassName(styles.square);
+
+    for (let i = 0; i < allSquares.length; i++) {
+      const square = allSquares[i];
+
+      square.style.border = "0px";
     }
 
-   
-}
+    if (clicked === false) {
+      setClicked(true);
+    } else {
+      setClicked(false);
+    }
+  };
 
+  const stopPreview = () => {
+    const allSquares = document.getElementsByClassName(styles.square);
 
-const handlePreview = () => {
-    const allSquares = document.getElementsByClassName(styles.square)
+    for (let i = 0; i < allSquares.length; i++) {
+      const square = allSquares[i];
 
-    for(let i = 0; i < allSquares.length; i++){
-       const square = allSquares[i]
-
-       square.style.border = '0px'
-       
+      square.style.border = "1px solid black";
     }
 
-    if(clicked === false){
-        setClicked(true)
-    }else{
-        setClicked(false)
+    if (clicked === false) {
+      setClicked(true);
+    } else {
+      setClicked(false);
     }
-}
+  };
 
-const stopPreview = () => {
-    const allSquares = document.getElementsByClassName(styles.square)
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <div className={styles.colorOptions}>
+            {choices.map((choice, index) => (
+              <div>
+                <div
+                  key={index}
+                  className={styles.colorBlock}
+                  style={{ backgroundColor: choice }}
+                  onClick={() => setColor(choice)}
+                ></div>
+                <br></br>
+              </div>
+            ))}
+            <br></br>
+            <p>Current Color:</p>
+            <div
+              className={styles.current}
+              style={{ backgroundColor: color }}
+            ></div>
+          </div>
 
-    for(let i =0; i < allSquares.length; i++){
-        const square = allSquares[i]
+          <div className={styles.box}>
+            {squares.map((squares, index) => (
+              <div
+                key={index}
+                className={styles.square}
+                id={`square-${index}`}
+                onClick={() => handleClick(index)}
+              ></div>
+            ))}
+          </div>
+        </div>
+        <br></br>
+      </div>
+      <div className={styles.buttonRow}>
+        {clicked ? (
+          <button onClick={stopPreview}>Stop Preview</button>
+        ) : (
+          <button onClick={handlePreview}>Preview</button>
+        )}
 
-        square.style.border = '1px solid black'
-    }
-
-    if(clicked === false){
-        setClicked(true)
-    }else{
-        setClicked(false)
-    }
-}
-
-return(
-<>
-
-
-
-<div className={styles.container}>
-<div className={styles.row}>
-<div className={styles.colorOptions}>
-    <div className={styles.white} onClick={ () => setColor('white')}></div>
-    <br></br>
-    <div className={styles.black} onClick={() => setColor('black')}></div>
-    <br></br>
-    <div className={styles.green} onClick={() => setColor('green')}></div>
-    <br></br>
-    <div className={styles.blue} onClick={() => setColor('blue')}></div>
-    <br></br>
-    <div className={styles.red} onClick={() => setColor('red')}></div>
-    <br></br>
-    <div className={styles.yellow} onClick={() => setColor('yellow')}></div>
-    <br></br>
-    <div className={styles.purple} onClick={() => setColor('purple')}></div>
-    <br></br>
-    <div className={styles.orange} onClick={() => setColor('orange')}></div>
-    <br></br>
-    <p>Current Color:</p>
-    <div className={styles.current} style={{backgroundColor: color}}></div>
-</div>
-
-<div className={styles.box}>
-    {squares.map((squares, index) => (
-        <div key={index} className={styles.square} id={`square-${index}`} onClick = {() => handleClick(index)}></div>
-    ))}
-</div>
-</div>
-<br></br>
-
-
-
-</div>
-<div className={styles.buttonRow}>
-{clicked ? <button onClick={stopPreview}>Stop Preview</button>  : <button onClick={handlePreview}>Preview</button>   }
-
-
-<button onClick={handleClear}>Clear</button>
-</div>
-
-
-    
-
-
-
-
-</>
-)
-}
+        <button onClick={handleClear}>Clear</button>
+      </div>
+    </>
+  );
+};
 
 export default Pad;
